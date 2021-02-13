f44f17cd75ceaac0645af8c2cdadf014e7ccddaa
drpriyabrat <78996854+drpriyabrat@users.noreply.github.com>
2021-02-13 12:11:31 +0530

    Add files via upload

---
+++ enter your important details.py
@@ -0,0 +1,10 @@
+# input Nationality
+country_name=input('Your Country Name:- ')
+state_name=input('Your State Name:- ')
+city_name=input('Your City Name:- ')
+pin_code=int(input('Your Postal Code:- '))
+
+print(f'= Your Country: {country_name}')
+print(f'= Your State: {state_name}')
+print(f'= Your City: {city_name}')
+print(f'= Your POSTAL CODE: {pin_code}')
